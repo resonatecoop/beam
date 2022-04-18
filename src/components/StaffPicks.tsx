9b7d6a0c751d754df5cbccf5d09527c2f1da445f
Simon <contact@vansintjan.net>
2022-04-18 06:19:19 -0400

    feat: staff picks randomize selection of playlists

--- src/components/StaffPicks.tsx
+++ src/components/StaffPicks.tsx
@@ -1,20 +1,42 @@
 import { css } from "@emotion/css";
+import { random } from "lodash";
 import React from "react";
+import { FaPlay } from "react-icons/fa";
+import { useGlobalStateContext } from "../contexts/globalState";
 
-import { fetchTrackGroup, fetchUserPlaylists } from "../services/Api";
+import { fetchTrackGroup } from "../services/Api";
+import Button from "./common/Button";
 import TrackList from "./common/TrackList";
 
+const staffPicks = [
+  "c4251486-680f-4cb9-8eed-87afccf5c29d",
+  "2e6d1aa3-cee0-4df9-8621-46e2c0c2993c",
+  "856e3176-e5e3-4d89-b5d9-a676258411b7",
+  "2bf25c61-e44b-4cde-b500-523ca38ec9d8",
+  "486e1abd-99f0-4acd-b1f0-01ebe36d3dda",
+];
+
 const StaffPicks: React.FC = () => {
-  const [latestStaffPick, setLatestStaffPick] = React.useState<Trackgroup>();
+  const { dispatch } = useGlobalStateContext();
+  const [latestStaffPick, setLatestStaffPick] =
+    React.useState<TrackgroupDetail>();
   const [tracks, setTracks] = React.useState<Track[]>();
 
   const fetchStaffPicksCallback = React.useCallback(async () => {
-    const result = await fetchUserPlaylists(12788, { limit: 1 });
-    setLatestStaffPick(result[0]);
+    const result = await fetchTrackGroup(staffPicks[random(staffPicks.length)]);
+    setLatestStaffPick(result);
 
-    const tracklisting = await fetchTrackGroup(result[0].id);
-    setTracks(tracklisting?.items.map((item) => item.track));
+    setTracks(result?.items.map((item) => item.track));
   }, []);
+
+  const onPlayClick = React.useCallback(() => {
+    if (tracks) {
+      dispatch({
+        type: "addTrackIdsToFrontOfQueue",
+        idsToAdd: tracks?.map((track) => track.id) ?? [],
+      });
+    }
+  }, [dispatch, tracks]);
 
   React.useEffect(() => {
     fetchStaffPicksCallback();
@@ -32,7 +54,33 @@ const StaffPicks: React.FC = () => {
         `}
       >
         <h3>Staff picks</h3>
-        <h4>{latestStaffPick.title}</h4>
+        <p
+          className={css`
+            margin-bottom: 1rem;
+          `}
+        >
+          Playlists by the people building Resonate showcasing some of their
+          favorite music
+        </p>
+        <div
+          className={css`
+            display: flex;
+            align-items: center;
+
+            > h4 {
+              margin-bottom: 0;
+              padding-bottom: 0;
+              padding-right: 1rem;
+            }
+          `}
+        >
+          <h4>{latestStaffPick.title}</h4>
+          {tracks && (
+            <Button compact startIcon={<FaPlay />} onClick={onPlayClick}>
+              Play
+            </Button>
+          )}
+        </div>
         {latestStaffPick.about}
       </div>
 
