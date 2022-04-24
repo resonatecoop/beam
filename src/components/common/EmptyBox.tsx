71db2e287b43eb840b5a6972614fd52994b67fb1
Simon <contact@vansintjan.net>
2022-04-24 19:47:31 -0400

    fix: clarify how to add things to the queue

--- src/components/common/EmptyBox.tsx
+++ src/components/common/EmptyBox.tsx
@@ -3,11 +3,17 @@ import styled from "@emotion/styled";
 const EmptyBox = styled.div`
   width: 100%;
   text-align: center;
-  height: 200px;
+  min-height: 300px;
+  flex-direction: column;
   display: flex;
   align-items: center;
   justify-content: center;
-  color: #777;
+  color: #999;
+  font-size: 1.4rem;
+
+  p {
+    margin-bottom: 1rem;
+  }
 `;
 
 export default EmptyBox;
