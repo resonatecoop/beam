08653b8391aed9696cc04fe8e0e6cd2f98e55488
Simon <contact@vansintjan.net>
2022-05-15 13:06:07 -0400

    fix: success message on adding a song to a playlist

--- src/components/AudioWrapper.tsx
+++ src/components/AudioWrapper.tsx
@@ -4,7 +4,7 @@ import { GlobalState, useGlobalStateContext } from "contexts/globalState";
 import React from "react";
 import AudioPlayer from "react-h5-audio-player";
 import { ImLoop } from "react-icons/im";
-import { getToken, registerPlay } from "../services/Api";
+import { registerPlay } from "../services/Api";
 
 import { buildStreamURL, getCORSSong } from "../utils/tracks";
 import IconButton from "./common/IconButton";
