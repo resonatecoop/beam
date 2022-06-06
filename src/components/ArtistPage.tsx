60c33a97744a488a55d126cae848ede752a02f80
Simon <contact@vansintjan.net>
2022-06-06 18:21:44 -0400

    fix: better empty state for artist headers without a profile photo

--- src/components/ArtistPage.tsx
+++ src/components/ArtistPage.tsx
@@ -1,4 +1,6 @@
 import { css } from "@emotion/css";
+import styled from "@emotion/styled";
+
 import React from "react";
 import { Link, useParams } from "react-router-dom";
 import {
@@ -15,6 +17,137 @@ import TrackTable from "./common/TrackTable";
 
 const padding = css`
   padding: 0 1rem 1rem;
+`;
+
+const bubbles = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: clip;
+
+  z-index: 1;
+
+  li {
+    position: absolute;
+    list-style: none;
+    display: block;
+    width: 40px;
+    height: 40px;
+    background-color: rgba(255, 255, 255, 0.2);
+    bottom: -160px;
+
+    -webkit-animation: square 25s infinite;
+    animation: square 25s infinite;
+
+    -webkit-transition-timing-function: linear;
+    transition-timing-function: linear;
+
+    &:nth-child(1) {
+      left: 10%;
+    }
+
+    &:nth-child(2) {
+      left: 20%;
+
+      width: 80px;
+      height: 80px;
+
+      animation-delay: 2s;
+      animation-duration: 17s;
+    }
+
+    &:nth-child(3) {
+      left: 25%;
+      animation-delay: 4s;
+    }
+
+    &:nth-child(4) {
+      left: 40%;
+      width: 60px;
+      height: 60px;
+
+      animation-duration: 22s;
+
+      background-color: fade(white, 25%);
+    }
+
+    &:nth-child(5) {
+      left: 70%;
+    }
+
+    &:nth-child(6) {
+      left: 80%;
+      width: 120px;
+      height: 120px;
+
+      animation-delay: 3s;
+      background-color: fade(white, 20%);
+    }
+
+    &:nth-child(7) {
+      left: 32%;
+      width: 160px;
+      height: 160px;
+
+      animation-delay: 7s;
+    }
+
+    &:nth-child(8) {
+      left: 55%;
+      width: 20px;
+      height: 20px;
+
+      animation-delay: 15s;
+      animation-duration: 40s;
+    }
+
+    &:nth-child(9) {
+      left: 25%;
+      width: 10px;
+      height: 10px;
+
+      animation-delay: 2s;
+      animation-duration: 40s;
+      background-color: fade(white, 30%);
+    }
+
+    &:nth-child(10) {
+      left: 90%;
+      width: 160px;
+      height: 160px;
+
+      animation-delay: 11s;
+    }
+  }
+
+  @-webkit-keyframes square {
+    0% {
+      transform: translateY(0);
+    }
+    100% {
+      transform: translateY(-700px) rotate(600deg);
+    }
+  }
+  @keyframes square {
+    0% {
+      transform: translateY(0);
+    }
+    100% {
+      transform: translateY(-700px) rotate(600deg);
+    }
+  }
+`;
+
+const TitleWrapper = styled.div`
+  min-height: 300px;
+  display: flex;
+  align-items: flex-end;
+  color: white;
+  margin-bottom: 1rem;
+  background-color: ${(props) => props.theme.colors.primary};
+  position: relative;
 `;
 
 export const ArtistPage: React.FC = () => {
@@ -54,6 +187,8 @@ export const ArtistPage: React.FC = () => {
     }
   }, [fetchTracks, artistId]);
 
+  const backgroundImage = artist?.images?.["cover_photo-l"];
+
   return (
     <div
       className={css`
@@ -63,18 +198,25 @@ export const ArtistPage: React.FC = () => {
       {isLoading && <CenteredSpinner />}
       {!isLoading && artist && (
         <>
-          <div
-            className={css`
-              min-height: 300px;
-              display: flex;
-              align-items: flex-end;
-              color: white;
-              margin-bottom: 1rem;
-            `}
+          <TitleWrapper
             style={{
-              backgroundImage: `url(${artist.images?.["cover_photo-l"]})`,
+              backgroundImage: `url(${backgroundImage})`,
             }}
           >
+            {!backgroundImage && (
+              <ul className={bubbles}>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            )}
             <h3
               className={css`
                 padding: 3rem 1rem 1rem;
@@ -87,7 +229,7 @@ export const ArtistPage: React.FC = () => {
             >
               {artist.name}
             </h3>
-          </div>
+          </TitleWrapper>
           <div className={padding}>
             <OverflowableText text={artist.bio} />
           </div>
