aaad18202a7fd62fb62893d1cbd2d88cdb371c27
Simon <contact@vansintjan.net>
2022-07-16 05:26:32 -0700

    fix: better flexibility on openid spec

--- src/auth/config.ts
+++ src/auth/config.ts
@@ -1,10 +1,13 @@
 import { WebStorageStateStore } from "oidc-client-ts";
 import { AuthProviderProps } from "./AuthContextInterface";
 
+const metadataUrl = process.env.REACT_APP_AUTH_METADATA_URL ?? undefined;
+
 export const oidcConfig: AuthProviderProps = {
-  authority: "https://id.resonate.coop",
+  authority: process.env.REACT_APP_AUTHORITY ?? "",
   clientId: process.env.REACT_APP_CLIENT_ID ?? "",
   redirectUri: "http://localhost:8080",
+  metadataUrl,
   clientSecret: process.env.REACT_APP_CLIENT_SECRET,
   scope: "read_write",
   client_authentication: "client_secret_basic",
@@ -12,12 +15,12 @@ export const oidcConfig: AuthProviderProps = {
   responseType: "code",
   automaticSilentRenew: true,
   userStore: new WebStorageStateStore({ store: window.localStorage }),
-  metadata: {
-    issuer: "https://id.resonate.coop",
-
-    authorization_endpoint: "https://id.resonate.coop/web/authorize",
-    token_endpoint: "https://id.resonate.coop/v1/oauth/tokens",
-    userinfo_endpoint: "",
-    // jwks_uri: "https://slack.com/openid/connect/keys",
-  },
+  metadata: metadataUrl
+    ? undefined
+    : {
+        issuer: process.env.REACT_APP_AUTHORITY,
+        authorization_endpoint: "https://id.resonate.coop/web/authorize",
+        token_endpoint: "https://id.resonate.coop/v1/oauth/tokens",
+        userinfo_endpoint: "",
+      },
 };
