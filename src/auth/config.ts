b052696f796dbebb87f6bcef8f8d8b1187ac0d4f
Si <simsamsomred@gmail.com>
2022-10-04 14:29:47 -0400

    fix: point to window location origin

--- src/auth/config.ts
+++ src/auth/config.ts
@@ -6,7 +6,7 @@ const metadataUrl = process.env.REACT_APP_AUTH_METADATA_URL ?? undefined;
 export const oidcConfig: AuthProviderProps = {
   authority: process.env.REACT_APP_AUTHORITY ?? "",
   clientId: process.env.REACT_APP_CLIENT_ID ?? "",
-  redirectUri: "http://localhost:8080",
+  redirectUri: window.location.origin,
   metadataUrl,
   clientSecret: process.env.REACT_APP_CLIENT_SECRET,
   scope: "read_write",
